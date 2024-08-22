7c3af2fe7388a084a72dcc032642650ba8979707
Sai Methish <Sai.Methish@godigit.com>
2024-08-22 17:51:42 +0530

    added dashboard for user and admin

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/service/user/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private userService:UserService, private router:Router) { }
+  userList:any=[];
+  userdept:string='';
   ngOnInit(): void {
+    let user:any=localStorage.getItem("user");
+    if(user==null){
+      user='';
+      this.router.navigate(['/login']);
+    }
+    else{
+      user=JSON.parse(user);
+      this.userdept=user.department;
+    }
+    this.userService.getUserList('/').subscribe({
+      next:(res:any)=>{
+        this.userList=res.filter((i:any)=>{
+          return i.isAdmin===false && i.department===this.userdept;
+        });
+        console.log("data from dashboard",this.userList)
+      },
+      error:err=>console.log(err),
+      complete:()=>{}
+    })
   }
 
 }

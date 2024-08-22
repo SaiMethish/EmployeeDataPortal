46d827b755023d86cf9ed365d23c701072650cdb
Sai Methish <Sai.Methish@godigit.com>
2024-08-22 11:41:14 +0530

    added routing for login,signup and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:DashboardComponent
+  },
+  {
+    path:'/signup',
+    component:SignUpComponent
+  },
+  {
+    path:'/login',
+    component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
